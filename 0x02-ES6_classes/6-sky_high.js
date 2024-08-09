bf8cdb1bc98e04dfb17e26233b3debdd4cd5172b
Pius Aaron <piuschbz@gmail.com>
2024-08-09 21:56:14 +0100

    Press on

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return(`Evacuate slowly the ${this.floors} floors`);
+  }
+}
+
+export default SkyHighBuilding;
