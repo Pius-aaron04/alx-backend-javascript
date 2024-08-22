fb72f86d34096cbf6127dc8c1ced62249b60c843
Pius Aaron <piuschbz@gmail.com>
2024-08-22 22:15:35 +0100

    Made changes to task 0 and 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,9 +6,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
-process.stdin.on('end', () =>{
-  console.log('This important software is now closing')
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
