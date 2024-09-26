a776c7e15c33535606c0806e8943a9b1c0e03cf3
Pius Aaron <piuschbz@gmail.com>
2024-09-26 16:01:38 +0100

    Added 100-evcar.js file to 0x02-ES6_classes file

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,30 @@
+import Car from './10-car';
+
+// 100-evcar.js
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new super.constructor();
+  }
+}
